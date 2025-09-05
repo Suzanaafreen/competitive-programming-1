d90da232165099b2b0927691a364d84bb8f2a4ad
Suzanaafreen <ksuzana992@gmail.com>
2025-09-05 22:15:49 +0600

    Create BC 1005.cpp

---
+++ BC 1005.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b;
+    cin>>fixed>>setprecision(1)>>a>>b;
+    a = a*3.5;
+    b = b*7.5;
+    cout<<fixed<<setprecision(5)<<"MEDIA = "<<(a+b)/11<<endl;
+    return 0;
+}
