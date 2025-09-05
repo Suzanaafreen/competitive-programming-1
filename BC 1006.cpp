0a27e68bcca5d9a97687adce33ba26eed183ed62
Suzanaafreen <ksuzana992@gmail.com>
2025-09-05 22:19:35 +0600

    Create BC 1006.cpp

---
+++ BC 1006.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cin>>a>>b>>c;
+    a = a*2;
+    b = b*3;
+    c = c*5;
+    cout<<fixed<<setprecision(1)<<"MEDIA = "<<(a+b+c)/10<<endl;
+    return 0;
+}
