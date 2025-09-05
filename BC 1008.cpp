8b3b0bea8e7bb3c934b8c2402ba9b183342aafd5
Suzanaafreen <ksuzana992@gmail.com>
2025-09-05 22:29:49 +0600

    Create BC 1008.cpp

---
+++ BC 1008.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    float c;
+    cin>>a>>b>>c;
+    cout<<"NUMBER = "<<a<<endl;
+    cout<<fixed<<setprecision(2)<<"SALARY = U$ "<<b*c<<endl;
+    return 0;
+}
