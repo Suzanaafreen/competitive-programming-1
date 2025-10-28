94e65f02fe032143567ec6a071a75d989fd51ab5
Suzanaafreen <ksuzana992@gmail.com>
2025-10-28 06:40:22 +0600

    Create CF-C-sheet2.cpp

---
+++ CF-C-sheet2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    int even=0,odd=0,pos=0,neg=0;
+
+    for(int i=1;i<=num;i++)
+    {
+        int n;
+        cin>>n;
+
+        if(n%2==0)
+            even++;
+        if(n%2!=0)
+            odd++;
+        if(n>0)
+            pos++;
+        if(n<0)
+            neg++;
+    }
+    cout<<"Even: "<<even<<endl;
+    cout<<"Odd: "<<odd<<endl;
+    cout<<"Positive: "<<pos<<endl;
+    cout<<"Negative: "<<neg<<endl;
+    return 0;
+}
