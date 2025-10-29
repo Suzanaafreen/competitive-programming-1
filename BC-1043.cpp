62ccd00ea53b05bcd08569c84911de45fb495327
Suzanaafreen <ksuzana992@gmail.com>
2025-10-29 11:13:58 +0600

    Create BC-1043.cpp

---
+++ BC-1043.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b,c,s;
+    cin>>a>>b>>c;
+    
+
+    if(a+b>c && b+c>a && a+c>b){
+        cout<<fixed<<setprecision(1)<<"Perimetro = "<<a+b+c<<endl;
+    }
+    
+    else{
+        cout<<fixed<<setprecision(1)<<"Area = "<<((a+b)*c)/2<<endl;
+    }
+    return 0;
+}
